72c1db9b2e89669a873f3e050704be0d02694208
Adriana <adrianacc20018@gmail.com>
2024-11-11 17:01:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función que se ejecuta cuando se hace clic en el botón
+function showMessage() {
+    // Muestra una alerta con un mensaje
+    alert("¡Gracias por contactarme! Me pondré en contacto contigo pronto.");
+}
